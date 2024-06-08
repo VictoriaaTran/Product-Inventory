aa7eb863d9f7b791d18f5c7b02023453c268f0a9
VictoriaaTran <victoriatrnn@gmail.com>
2024-06-07 18:43:48 -0700

    insert statements

---
+++ Insert.sql
@@ -0,0 +1,16 @@
+-- 2. Insert statements
+
+-- Insert into Retailer
+INSERT INTO Retailer(Retailer_Id, Retailer_Name, Address1, City, State, Zip)
+VALUES (2222222222, 'Walmart', '12345 Pace Landing', 'Brandon', 'MO', '15673');
+
+INSERT INTO Retailer(Retailer_Id, Retailer_Name, Address1, City, State, Zip)
+VALUES (2222222223, 'Target', '12346 Ledo Way Landing', 'Lakewood', 'MI', '15674');
+
+INSERT INTO Retailer(Retailer_Id, Retailer_Name, Address1, City, State, Zip)
+VALUES (2222222224, 'Best Buy', '356 First St', 'Ransome', 'NB', '15675' );
+
+INSERT INTO Retailer(Retailer_Id, Retailer_Name, Address1, City, State, Zip)
+VALUES (2222222225, 'Netflix', '546 Beverely Dr', 'Hollywood', 'CA', '15676');
+
+
